14128a1b9ab9e6fd4b8603e1472a7c1b836d999c
xCr1s12 <CristobalAlexis.onate@alumnos.ulagos.cl>
2024-05-04 14:00:37 -0400

    Primer Commit#1

---
+++ saludar.py
@@ -0,0 +1,15 @@
+
+nombre = "Cristobal" 
+print("Hola soy ", nombre)
+edad = int(input("Introduce tu Edad: ")) 
+print (nombre," mi edad es: ", edad)
+
+name = "Sancho"
+print(f"hola soy {name}") 
+Años = int(input("Introduce tu edad: "))
+print (f"{name} tu edad es {Años}") 
+
+nmbre = "pavlosky"
+print("Hola soy "+nmbre)
+añitos = str(input("introduce tu edad: ")) 
+print(" tu edad es: " + añitos)
