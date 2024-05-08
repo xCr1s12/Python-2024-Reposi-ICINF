862c96e3c75d8912a3e9e16d6f650fa7e5c52da6
xCr1s12 <CristobalAlexis.onate@alumnos.ulagos.cl>
2024-05-08 16:24:24 -0400

    Reto

---
+++ Retos/Reto-N1.py
@@ -0,0 +1,9 @@
+"""
+ALGORITMO MENSAJE
+"""
+
+
+nombre= input("Ingresa tu nombre: ")
+Edad = int(input("Ingresa tu edad: "))
+años_mas= Edad + 20
+print(f"Hola mi nombre es {nombre} y tengo {Edad} años pero en 20 años tendre {años_mas}  ")
--- Variables.py
+++ Variables.py
@@ -2,15 +2,16 @@
 
 name = "Cristobal" 
 edad = 12
+apellido= input("Escribe tu apellido")
  
 #1 Clasico 
 print("Hola soy ", name)
 print (name," mi edad es: ", edad)
 
-#2 f string
+
+#2 f-string
 print(f"hola soy {name}") 
 print (f"{name} tu edad es {edad}") 
-
 #3 concatenar
 print("Hola soy "+name)
 print(" mi edad es  " + str(edad)) 
