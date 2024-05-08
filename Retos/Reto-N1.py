eed4e02433a289517e7ade08e1fc69877ba60cb9
xCr1s12 <CristobalAlexis.onate@alumnos.ulagos.cl>
2024-05-08 16:27:36 -0400

    Reto

--- Retos/Reto-N1.py
+++ Retos/Reto-N1.py
@@ -6,4 +6,4 @@ ALGORITMO MENSAJE
 nombre= input("Ingresa tu nombre: ")
 Edad = int(input("Ingresa tu edad: "))
 años_mas= Edad + 20
-print(f"Hola mi nombre es {nombre} y tengo {Edad} años pero en 20 años tendre {años_mas}  ")
+print(f"Hola mi nombre es {nombre} y tengo {Edad} años pero en 20 años tendre {años_mas} años")
