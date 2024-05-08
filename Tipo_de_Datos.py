3df88e5f246431808a7271c981f859f81cf52238
xCr1s12 <CristobalAlexis.onate@alumnos.ulagos.cl>
2024-05-08 16:06:53 -0400

    Datasos

--- Tipo_de_Datos.py
+++ Tipo_de_Datos.py
@@ -5,3 +5,15 @@ Asignaturas = """
 Taller Introduccion ing civil inf
 Programacion
 """ 
+
+
+#INDICES 
+print(Dia[3]) # imprimira el caracter en la posicion 4 ya que cuenta desde el 0 
+print(Dia[-3]) #Imprimira a la inversa desde el 0   
+"""
+print(Dia * 4)
+Salida de texto va a ser LunesLunesLunesLunes 
+"""
+pi=3.14159
+print(len(Dia))
+print("{:.2f}".format(pi))
